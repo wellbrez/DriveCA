e489848334e605060de7f60b09a805730e44bf9c
wellbrez <bld-wellington@hotmail.com>
2021-08-16 21:49:41 -0300

    change xmlhttprequest to fetch

--- index.js
+++ index.js
@@ -1,63 +1,69 @@
 const Planilha = [];
 
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    let data = JSON.parse(this.responseText).feed.entry;
+fetch(
+  "https://spreadsheets.google.com/feeds/list/1zQlECCi0lONoDG794Q4E5pDeL0d5DqKw3sQEkjYdSd8/onlvse5/public/values?alt=json"
+)
+  .then((response) => response.json())
+  .then((resposta) => {
+    let data = resposta.feed.entry;
+    console.log(data);
     let i;
     for (i = 0; i < data.length; i++) {
       const materia = data[i]["gsx$matéria"]["$t"];
       const descricao = data[i]["gsx$descriçãocurtadomaterialenviado"]["$t"];
       const tipoDeArquivo = data[i]["gsx$tipodearquivo"]["$t"];
       const urlArquivo = data[i]["gsx$envieoarquivoaqui"]["$t"];
-      const periodo = data[i]["gsx$dataemqueomaterialfoidisponibilizado"]["$t"]
-      const dataEnvio = data[i]["gsx$carimbodedatahora"]["$t"]
-      const nomeDaOptativa = data[i]["gsx$nomedamatériaapenassenãoestiverpresentenalistaacimaoptativaseoutras"]["$t"]
-      
-      Planilha.push({materia,tipoDeArquivo,urlArquivo,periodo,dataEnvio,nomeDaOptativa,descricao})
+      const periodo = data[i]["gsx$dataemqueomaterialfoidisponibilizado"]["$t"];
+      const dataEnvio = data[i]["gsx$carimbodedatahora"]["$t"];
+      const nomeDaOptativa =
+        data[i][
+          "gsx$nomedamatériaapenassenãoestiverpresentenalistaacimaoptativaseoutras"
+        ]["$t"];
+
+      Planilha.push({
+        materia,
+        tipoDeArquivo,
+        urlArquivo,
+        periodo,
+        dataEnvio,
+        nomeDaOptativa,
+        descricao,
+      });
     }
-    popularTabela(Planilha,"planilha");
+    popularTabela(Planilha, "planilha");
     console.log(pegaQtdEmUmaSemana());
-  }
-  if(this.readyState==4 && this.status!=200)
-  {
-    window.alert("Houve um erro com a obtenção de dados do drive. Tente reiniciar a página ou verifique sua conexão com a internet");
-  }
-};
+    return;
+  })
+  .catch((error) => {
+    window.alert(
+      "Houve um erro com a obtenção de dados do drive. Tente reiniciar a página ou verifique sua conexão com a internet"
+    );
+  });
 
-xmlhttp.open(
-  "GET",
-  "https://spreadsheets.google.com/feeds/list/1zQlECCi0lONoDG794Q4E5pDeL0d5DqKw3sQEkjYdSd8/onlvse5/public/values?alt=json",
-  true
-);
-xmlhttp.send();
+function popularTabela(Planilha, idElemento) {
+  if (!Planilha) return;
+  planilha = document.getElementById(idElemento);
+  planilha.innerHTML =
+    "<tr><th>Matéria</th><th>Período</th><th>Tipo de Arquivo</th><th>Link</th><th>Data de envio</th></tr>";
+  for (let elm of Planilha) {
+    let materia = elm.nomeDaOptativa ? elm.nomeDaOptativa : elm.materia;
 
-
-function popularTabela(Planilha,idElemento)
-{
-    if(!Planilha)return;
-    planilha = document.getElementById(idElemento);
-    planilha.innerHTML = "<tr><th>Matéria</th><th>Período</th><th>Tipo de Arquivo</th><th>Link</th><th>Data de envio</th></tr>";
-    for(let elm of Planilha)
-    {
-      let materia = (elm.nomeDaOptativa)?elm.nomeDaOptativa:elm.materia;
-
-        planilha.innerHTML+=`<tr>
+    planilha.innerHTML += `<tr>
         <td>${materia}</td>
         <td>${elm.periodo}</td>
         <td>${elm.tipoDeArquivo}</td>
         <td>${transformarURLEmIcone(elm)}</td>
         <td>${elm.dataEnvio}</td>
         </tr>
-        `
-    }
+        `;
+  }
 }
-let linkDriveAntigo = document.getElementsByClassName("driveAntigo")
-for(let link of linkDriveAntigo)
-{
-  link.onclick=popupDriveAntigo;
+let linkDriveAntigo = document.getElementsByClassName("driveAntigo");
+for (let link of linkDriveAntigo) {
+  link.onclick = popupDriveAntigo;
 }
-function popupDriveAntigo()
-{
-  alert("Hey, se você achar o arquivo lá, faz upload nessa nova plataforma por favor!! Agradecemoss!")
+function popupDriveAntigo() {
+  alert(
+    "Hey, se você achar o arquivo lá, faz upload nessa nova plataforma por favor!! Agradecemoss!"
+  );
 }
